fae62b2a188a07597ba70cd6657338ea33b5c4f5
SEIFU MEKONNEN TOLOSA <tms.0xvoid@gmail.com>
2022-08-30 16:10:52 +0100

    clean up

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
-
